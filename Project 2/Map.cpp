8caebe50867bda44af2b8342d62f890c613e1317
Margaret Capetz <57432165+mcapetz@users.noreply.github.com>
2022-01-21 21:40:14 -0800

    started implementing functions

---
+++ Project 2/Map.cpp
@@ -0,0 +1,150 @@
+//
+//  Map.cpp
+//  CS32-Proj2
+//
+//  Created by Margaret Capetz on 1/21/22.
+//
+
+// Map.cpp
+
+#include "Map.h"
+#include <iostream>
+
+using namespace std;
+
+Map::Map()
+{
+    
+}
+
+bool Map::erase(const KeyType& key)
+{
+    if(findFirstAtLeast(key) == nullptr) {
+        //not found, cannot erase
+        return false;
+    }
+    
+    //found
+    linkedList::Node* killMe = findFirstAtLeast(key);
+    linkedList::Node* prevNode = killMe->prev;
+    linkedList::Node* nextNode = killMe->next;
+    
+    prevNode->next = nextNode;
+    nextNode->prev = prevNode;
+
+    linkedList.m_size--;
+    return true;
+}
+
+bool Map::get(const KeyType& key, ValueType& value) const
+{
+    if(findFirstAtLeast(key) == nullptr) return false;
+    linkedList::Node* n = findFirstAtLeast(key);
+    value = n->pair.m_value;
+    
+    return true;
+}
+
+bool Map::get(int i, KeyType& key, ValueType& value) const
+{
+    if (i < 0  ||  i >= linkedList.m_size) return false;
+    
+    linkedList::Node* p = linkedList.dummy->next;
+    for(int j = 0; j < i; j++) {
+        p = p->next;
+    }
+    key = p->pair.m_key;
+    value = p->pair.m_value;
+    
+    return true;
+}
+
+void Map::swap(Map& other)
+{
+//      // Swap elements.  Since the only elements that matter are those up to
+//      // m_size and other.m_size, only they have to be moved.
+//
+//    int minSize = (m_size < other.m_size ? m_size : other.m_size);
+//    for (int k = 0; k < minSize; k++)
+//    {
+//        Pair tempPair = m_data[k];
+//        m_data[k] = other.m_data[k];
+//        other.m_data[k] = tempPair;
+//    }
+//
+//      // If the sizes are different, assign the remaining elements from
+//      // the longer one to the shorter.
+//
+//    if (m_size > minSize)
+//        for (int k = minSize; k < m_size; k++)
+//            other.m_data[k] = m_data[k];
+//    else if (other.m_size > minSize)
+//        for (int k = minSize; k < other.m_size; k++)
+//            m_data[k] = other.m_data[k];
+//
+//      // Swap sizes
+//
+//    int t = m_size;
+//    m_size = other.m_size;
+//    other.m_size = t;
+}
+
+Map::linkedList::Node* Map::findFirstAtLeast(const KeyType& key) const
+{
+    linkedList::Node* p = linkedList.dummy->next;
+    while(p != linkedList.dummy) {
+        if(key == p->pair.m_key) {
+            return p;
+        }
+        p = p->next;
+    }
+
+    return nullptr;
+}
+
+bool Map::doInsertOrUpdate(const KeyType& key, const ValueType& value)
+{
+
+    if(findFirstAtLeast(key) != nullptr) { //found
+        //update
+        linkedList::Node* n = findFirstAtLeast(key);
+        n->pair.m_key = key;
+        n->pair.m_value = value;
+    }
+    
+    else { //not found
+        //insert in SORTED ORDER
+        linkedList::Node* p = linkedList.dummy->next;
+        while(p != linkedList.dummy) {
+            if(p->pair.m_key > key) {
+                //reached the first Node that has a pair greater than p pair
+                //insert here
+                break;
+            }
+            p = p->next;
+        }
+        
+        //insert
+        linkedList::Node* n = new linkedList::Node();
+        n->pair.m_key = key;
+        n->pair.m_value = value;
+        
+        linkedList::Node* prevNode = p->prev; //order should be prevNode -> n -> p
+        prevNode->next = n;
+        n->next = p;
+        p->prev = n;
+        n->prev = prevNode;
+        
+        linkedList.m_size++;
+    }
+
+    return true;
+}
+
+void Map::dump() const {
+    linkedList::Node* p = linkedList.dummy->next;
+    while(p != linkedList.dummy) {
+        cerr << p->pair.m_key  << ": " << p->pair.m_value << endl;
+        p = p->next;
+    }
+}
---
+++ Project 2/Map.h
@@ -0,0 +1,173 @@
+//
+//  Map.hpp
+//  CS32-Proj2
+//
+//  Created by Margaret Capetz on 1/21/22.
+//
+
+// Map.h
+
+#ifndef MAP_INCLUDED
+#define MAP_INCLUDED
+
+  // Later in the course, we'll see that templates provide a much nicer
+  // way of enabling us to have Maps of different types.  For now, we'll
+  // use type aliases.
+
+#include <string>
+
+using KeyType = std::string;
+using ValueType = double;
+
+const int DEFAULT_MAX_ITEMS = 260;
+
+class Map
+{
+  public:
+    Map();         // Create an empty map (i.e., one with no key/value pairs)
+
+    bool empty() const;  // Return true if the map is empty, otherwise false.
+
+    int size() const;    // Return the number of key/value pairs in the map.
+
+    bool insert(const KeyType& key, const ValueType& value);
+      // If key is not equal to any key currently in the map, and if the
+      // key/value pair can be added to the map, then do so and return true.
+      // Otherwise, make no change to the map and return false (indicating
+      // that either the key is already in the map, or the map has a fixed
+      // capacity and is full.
+
+    bool update(const KeyType& key, const ValueType& value);
+      // If key is equal to a key currently in the map, then make that key no
+      // longer map to the value that it currently maps to, but instead map to
+      // the value of the second parameter; return true in this case.
+      // Otherwise, make no change to the map and return false.
+
+    bool insertOrUpdate(const KeyType& key, const ValueType& value);
+      // If key is equal to a key currently in the map, then make that key no
+      // longer map to the value that it currently maps to, but instead map to
+      // the value of the second parameter; return true in this case.
+      // If key is not equal to any key currently in the map, and if the
+      // key/value pair can be added to the map, then do so and return true.
+      // Otherwise, make no change to the map and return false (indicating
+      // that the key is not already in the map and the map has a fixed
+      // capacity and is full).
+
+    bool erase(const KeyType& key);
+      // If key is equal to a key currently in the map, remove the key/value
+      // pair with that key from the map and return true.  Otherwise, make
+      // no change to the map and return false.
+     
+    bool contains(const KeyType& key) const;
+      // Return true if key is equal to a key currently in the map, otherwise
+      // false.
+     
+    bool get(const KeyType& key, ValueType& value) const;
+      // If key is equal to a key currently in the map, set value to the
+      // value in the map which that key maps to and return true.  Otherwise,
+      // make no change to the value parameter of this function and return
+      // false.
+     
+    bool get(int i, KeyType& key, ValueType& value) const;
+      // If 0 <= i < size(), copy into the key and value parameters the
+      // key and value of the key/value pair in the map whose key is strictly
+      // greater than exactly i keys in the map and return true.  Otherwise,
+      // leave the key and value parameters unchanged and return false.
+
+    void swap(Map& other);
+      // Exchange the contents of this map with the other one.
+    
+    void dump() const;
+
+  private:
+
+      // Since this structure is used only by the implementation of the
+      // Map class, we'll make it private to Map.
+
+    struct Pair
+    {
+        KeyType   m_key;
+        ValueType m_value;
+    };
+    
+    struct linkedList {
+        struct Node {
+            Pair pair;
+            Node* next;
+            Node* prev;
+            
+            Node() {
+                next = nullptr;
+                prev = nullptr;
+            }
+        };
+        
+        Node* dummy;
+        int m_size;
+        
+        linkedList() {
+            dummy = new Node();
+            dummy->next = dummy; //head
+            dummy->prev = dummy; //tail
+            m_size = 0;
+        }
+    };
+    
+    
+
+    linkedList linkedList;  // the pairs in the map
+    //int  m_size;                     // number of pairs in the map
+
+      // At any time, the elements of m_data indexed from 0 to m_size-1
+      // are in use and are stored in ascending order of the keys.
+
+    linkedList::Node* findFirstAtLeast(const KeyType& key) const;
+      // Return the index of the pair in m_data whose key is the least one
+      // that is >= key, or m_size if there is no such pair.
+
+    bool doInsertOrUpdate(const KeyType& key, const ValueType& value);
+          
+};
+
+// Inline implementations
+
+inline
+int Map::size() const
+{
+    return linkedList.m_size;
+}
+
+inline
+bool Map::empty() const
+{
+    return linkedList.m_size == 0;
+}
+
+inline
+bool Map::contains(const KeyType& key) const
+{
+    if(findFirstAtLeast(key) == nullptr) return false;
+    return true;
+}
+
+inline
+bool Map::insert(const KeyType& key, const ValueType& value)
+{
+    if(findFirstAtLeast(key) != nullptr) return false; //cannot insert if it is already in the map
+    return doInsertOrUpdate(key, value);
+}
+
+inline
+bool Map::update(const KeyType& key, const ValueType& value)
+{
+    if(findFirstAtLeast(key) == nullptr) return false; //cannot update if not in map
+    return doInsertOrUpdate(key, value);
+}
+
+inline
+bool Map::insertOrUpdate(const KeyType& key, const ValueType& value)
+{
+    return doInsertOrUpdate(key, value);
+}
+
+#endif // MAP_INCLUDED
