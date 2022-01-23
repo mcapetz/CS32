2e22c478c9f6bb4ff3d3ed72c14d08329ace9b0e
Margaret Capetz <57432165+mcapetz@users.noreply.github.com>
2022-01-23 08:42:10 -0800

    updated testing

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #include <iostream>
 #include <cassert>
 
-void test1() {
+void test1() { //basic functions
     cerr << "test 1 start" << endl;
     Map m;
     assert(m.size() == 0); //assert initial size is zero
@@ -38,7 +38,7 @@ void test1() {
     cerr << "test 1 passed" << endl;
 }
 
-void test2() {
+void test2() { //insert/update functions
     cerr << "test 2 start" << endl;
     Map m;
     m.insert("a" , 1);
@@ -54,7 +54,7 @@ void test2() {
     cerr << "test 2 passed" << endl;
 }
 
-void test3() {
+void test3() { //2nd get function
     cerr << "test 3 start" << endl;
     Map m;
     m.insert("z", 1);
@@ -69,6 +69,18 @@ void test3() {
     assert(m.get(-1, key, val) == false && key == "a" && val == 3); //if i < 0 then false, key/value pair should not be altered
     assert(m.get(4, key, val) == false && key == "a" && val == 3); //if i >= size then false, key/value pair should not be altered
     assert(m.get(1, key, val) == true && key == "b" && val == 4); //the key strictly greater than 1 key is "b" the second key
+    
+    Map msd;  // KeyType is std::string, ValueType is double
+            msd.insert("ccc", 80);
+            msd.insert("aaa", 40);
+            msd.insert("ddd", 60);
+            msd.insert("bbb", 20);
+            KeyType k = "xxx";
+            ValueType v;
+            assert(!msd.get(4, k, v)  &&  k == "xxx");  // x is unchanged
+            assert(msd.get(1, k, v)  &&  k == "bbb");   // "bbb" is greater than
+                                                        // exactly 1 item
+    
     cerr << "test 3 passed" << endl;
 }
 
@@ -102,11 +114,65 @@ void test4() {
     l.dump();
     cerr << "test 4 passed" << endl;
 }
+
+
+void test5() { //merge function
+    cerr << "test 5 start" << endl;
+    Map m;
+    m.insert("fred", 123);
+    m.insert("ethel", 456);
+    m.insert("lucy", 789);
+    
+    Map n;
+    n.insert("lucy", 789);
+    n.insert("ricky", 321);
+    
+    Map o;
+    o.insert("lucy", 000);
+    o.insert("ricky", 321);
+    
+    Map p; //empty
+    
+    Map result;
+    assert(merge(m, n, result) == true); //merge returns true because it doesn't run into the case in which a key has two unique vals in both maps
+//    cerr << "result" << endl;
+//    result.dump();
+//    cerr << "result end" << endl;
+    
+    Map result2;
+    assert(merge(m, o, result2) == false); //merge returns false because one key has two vals in both maps
+    assert(result2.contains("ethel") == true);
+    assert(result2.contains("fred") == true);
+    assert(result2.contains("ricky") == true); //should not contain lucy because two lucy keys had 2 different values between the two maps
+//    cerr << "result 2" << endl;
+//    result2.dump();
+//    cerr << "result 2 end" << endl;
+    
+    Map result3;
+    assert(merge(m, p, result3) == true); //merge returns true because merging a non-empty and empty map should be successful
+    assert(result3.contains("fred") == true);
+    assert(result3.contains("ethel") == true);
+    assert(result3.contains("lucy") == true); //assert that result contains contents of Map m
+//    cerr << "result 3" << endl;
+//    result3.dump();
+//    cerr << "result 3 end" << endl;
+    
+    Map x;
+    Map y;
+    Map result4;
+    assert(merge(x, y, result4) == true);
+    assert(result4.empty() == true);
+   
+    cerr << "test 5 passed" << endl;
+}
+
 //
 //void test4() {
 //    cerr << "test 4 start" << endl;
 //    cerr << "test 4 passed" << endl;
 //}
+
+
 
 int main(int argc, const char * argv[]) {
     
@@ -114,6 +180,7 @@ int main(int argc, const char * argv[]) {
     test2();
     test3();
     test4();
+    test5();
     
     
 }
